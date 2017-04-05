290bbcb03d80d4c4632c5088a08368b0548b5374
Artem <klufone@mail.ru>
2017-04-05 18:06:58 +0300

    read the bookto work with it

--- app/main.cpp
+++ app/main.cpp
@@ -1,17 +1,22 @@
 #include <iostream>
+#include <fstream> 
 #include "../src/rabinPark.cpp"
+#define pathToTheFile "../filesToRead/WP.txt"
 
-int main(int argc, char const *argv[])
-{
-	std::string ourSubString = " ";
-	std::cout << "Enter your substring" << std::endl;
+
+int main(int argc, char const *argv[]){
+	std::string ourSubString,str,strTotal;
+	std::ifstream in;
+	in.open(pathToTheFile);
+	while ( in ) {
+		getline(in,str);
+ 	  	strTotal += str;
+	}
+
+	std::cout <<"You work with War and Peace" << std::endl 
+			  << "Enter desired substring" << std::endl;
 	std::cin >> ourSubString;
-	int result = RabinKarp("Thesimplesentence,whereyoucanfindyoursubstring",ourSubString);
-	if (result) {
-		std::cout << result <<std::endl;
-	}
-	else {
-		std::cout << "Not found" <<std::endl;
-	}
+	int result = RabinKarp(strTotal,ourSubString);
+	(result != 0) ? std::cout << result <<std::endl  : std::cout << "Not found" <<std::endl;
 	return 0;
 }
