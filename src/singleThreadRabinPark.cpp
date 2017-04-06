3503768d54b17b5ae9d74545b735134d642d3e1f
Artem <klufone@mail.ru>
2017-04-07 00:20:18 +0300

    rename file

--- src/singleThreadRabinPark.cpp
+++ src/singleThreadRabinPark.cpp
@@ -1,16 +1,14 @@
 #include <string>
 #include "hashFunc.cpp"
-int RabinKarp(std::string mainStr, std::string sub) {
-	const int subLength = sub.size();
+int singleThreadRabinPark(std::string mainStr, std::string sub) {
+	int result = 0;
 	long desiredSubStrHash = hashFunc(sub);
 	long hs = hashFunc(mainStr.substr(0,sub.size()) );
-	for (int i = 0;i < mainStr.size()-sub.size();i++){
-	   	if (hs == desiredSubStrHash ){
-	       	if (mainStr.substr(i,subLength) == sub ){
-	           	return i;
-	       	}
+	for (int i = 0; i < mainStr.size()-sub.size();i++){
+	   	if ((hs == desiredSubStrHash ) && (mainStr.substr(i,sub.size()) == sub )){
+			result++;
 	   	}
-	   	hs = hashFunc(mainStr.substr(i+1,subLength ));
+	   	hs = hashFunc(mainStr.substr(i+1,sub.size() ));
 	   }
-return 0;
+	return result;
 }
