3847b514617d374b2e0103c787c8bdc73f416bb9
Artem <klufone@mail.ru>
2017-04-07 00:19:57 +0300

    reworked parallel alg

--- src/rabinPark.cpp
+++ src/rabinPark.cpp
@@ -1,16 +1,49 @@
 #include <string>
-#include "hashFunc.cpp"
-int RabinKarp(std::string mainStr, std::string sub) {
-	const int subLength = sub.size();
-	long desiredSubStrHash = hashFunc(sub);
-	long hs = hashFunc(mainStr.substr(0,sub.size()) );
-	for (int i = 0;i < mainStr.size()-sub.size();i++){
-	   	if (hs == desiredSubStrHash ){
-	       	if (mainStr.substr(i,subLength) == sub ){
-	           	return i;
-	       	}
-	   	}
-	   	hs = hashFunc(mainStr.substr(i+1,subLength ));
-	   }
-return 0;
+#include <ctime>
+#include <vector>
+#include <omp.h>
+#include "singleThreadRabinPark.cpp"
+
+//omp_get_thread_num - number of thread
+//omp_get_num_threads() - all threads count
+int RabinKarp(std::string mainStr, std::string sub,int NUMBER_OF_THREADS) {
+	int num = 0;
+	int lastPlotSize = 0,lastThreadSize = 0;
+	const clock_t begin_time = clock();
+	const long desiredSubStrHash = hashFunc(sub);
+	const int numberOfPlots = mainStr.size() / sub.size();
+	
+	(mainStr.size() % sub.size())  ?
+			lastPlotSize = mainStr.size() % sub.size() + sub.size() :
+			lastPlotSize = sub.size();
+	#pragma omp parallel reduction(+:num) num_threads(NUMBER_OF_THREADS) 
+	{	
+		const int threadsDiap = numberOfPlots / omp_get_num_threads();
+
+		(numberOfPlots % omp_get_num_threads()) ?
+				lastThreadSize = threadsDiap + numberOfPlots % omp_get_num_threads() :
+				lastThreadSize = threadsDiap;
+
+		if (omp_get_num_threads() == 1) {
+			num = singleThreadRabinPark(mainStr,sub);
+		}
+		else {
+			#pragma parallel for num_threads(NUMBER_OF_THREADS)
+			for (int i = 0; i < sub.size() * threadsDiap ; i++)
+			{
+				long hs;
+				if (!i) {
+					hs = hashFunc(mainStr.substr(0,sub.size()) );
+				}
+				int startPos = sub.size() * threadsDiap * omp_get_thread_num() + i ;
+				if ((hs == desiredSubStrHash )&&(mainStr.substr(startPos,sub.size()) == sub )){
+			       	#pragma omp atomic
+			        num++;
+			   	}
+			   	hs = hashFunc(mainStr.substr(startPos+1,sub.size() ));	
+			}
+		}
+	}
+	printf("time =  %f sec\n" ,(float(clock () - begin_time ) / CLOCKS_PER_SEC)); 
+	return num;
 }
